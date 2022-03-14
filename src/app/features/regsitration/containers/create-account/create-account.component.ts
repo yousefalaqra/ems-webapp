3bd0c3adf2c159c435a4f9bc0b2e66885b503bd3
LoaiMasri <s12010098@stu.najah.edu>
2022-03-14 22:55:20 +0200

    form design , on submit http request

--- src/app/features/regsitration/containers/create-account/create-account.component.ts
+++ src/app/features/regsitration/containers/create-account/create-account.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, Validators,FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.scss']
 })
-export class CreateAccountComponent implements OnInit {
+export class CreateAccountComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor(private _HttpClient :HttpClient) { }
 
   ngOnInit() {
   }
+  onSubmit():void{
+    console.log(this.form.value);
+    this._HttpClient.post('http://localhost:3000/api/v1/user', this.form.value)
+    .subscribe((data) => {
+        console.log(data);
+      });
+    this.form.reset();
+  }
 
+  ngOnDestroy(){
+
+  }
 }
--- src/app/features/regsitration/regsitration.module.ts
+++ src/app/features/regsitration/regsitration.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RegistrationComponent } from './components/regsitration/regsitration.component';
 import { RegistrationRoutingModule } from './regsitration.routes';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { CreateAccountComponent } from './containers/create-account/create-account.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [RegistrationComponent],
+  declarations: [RegistrationComponent,CreateAccountComponent],
   providers: [],
-  imports: [RegistrationRoutingModule],
+  imports: [RegistrationRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
 })
 export class RegistrationModule {}
